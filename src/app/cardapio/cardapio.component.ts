793d389d56f65a5ee482d263c7f8c123f2d04b2a
Danilo Abrantes <113843354+daniloabrantes@users.noreply.github.com>
2022-11-27 22:49:46 -0300

    Update cardapio.component.ts

--- src/app/cardapio/cardapio.component.ts
+++ src/app/cardapio/cardapio.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cardapio',
   templateUrl: './cardapio.component.html',
-  styleUrls: ['./cardapio.component.css']
+  styleUrls: ['./cardapio.component.css'],
 })
 export class CardapioComponent implements OnInit {
   constructor() {}
